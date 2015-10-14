3b425b57f1438c939bcbaa983bb3be220494adeb
Dave Stearns <dlsinfo@uw.edu>
2015-10-14 12:38:46 -0700

    file stubs for calculation exercise

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # javascript-lab
-File stubs for JavaScript lab exercises
+
+Fork this repo to your own account, and clone it to your machine.
+
+The HTML and CSS are already implemented, so you will be practicing writing the JavaScript to make the example function. Follow the instructions in the comments in each JavaScript file.
+
---
+++ js/calculation.js
@@ -0,0 +1,45 @@
+/*
+    script for the calculation.html page
+
+    First, look over the calculation.html page in your editor. Note what elements are there
+    and what their IDs are. You can also open the page in your browser to see what it
+    looks like.
+
+    Your job is to write the code necessary to catch the click event on the "=" button,
+    add together the numbers entered into the <input id="value-1"> and <input id="value-2">
+    boxes, and display the result in the <span id="result"> element.
+
+    Here are some hints and links to references:
+    - Look at the code we wrote together on Tuesday. We did very similar things.
+    
+    - Remember that you should wait until the DOMContentLoaded event raised by the
+      document object before you start asking for elements by their IDs. See
+      https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+
+    - Remember that you can use document.getElementById() to get a reference to
+      an element given its unique ID value. Make sure you spell the ID correctly.
+      If you're using WebStorm, hit ctrl+space inside the parameter list to get
+      a completion drop down with all the IDs on the page.
+      https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
+
+    - You can get the value of an input element using the .value property.
+      But remember that this value will be a string.
+      http://www.w3schools.com/jsref/prop_text_value.asp.
+
+    - You can convert a string into an number using either parseInt() or 
+      parseFloat(), depending on whether you want an integer (no decimals)
+      or a float (with decimals).
+
+    - You can change the contents of an element by setting its .textContent property
+      https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent
+
+    - After you get the basic case working, you should also write the code to handle
+      following error conditions:
+      - user clicks = without typing in one or both of the values
+      - the user entered values but they are not numbers (e.g., "xyz")
+      You can use the <p id="error-message"> paragraph to display an error message.
+      This is hidden by default (see css/main.css), but you can write the code to
+      set its .textContent property with your error message, and set 
+      .style.display = 'block' to make it visible. Remember to hide it again
+      after the user corrects the problem.
+*/
